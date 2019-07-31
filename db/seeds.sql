c48cb8bd1834ea0cc7339ee9a1c60af16ef15f71
PWFollett <pwf592110@gmail.com>
2019-07-31 15:23:11 -0400

    trying to finish

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO burgers(burger_name, devoured, date)
 VALUES ("Big Mac", false, "2019-07-26 04:20:00"),
-  ("Whooper", false, "2019-07-27 04:20:01"),
-  ("Cheezburger", false, "2019-07-29 04:20:02"),
+  ("Whopper", false, "2019-07-27 04:20:01"),
+  ("Double Double", false, "2019-07-29 04:20:02"),
   ("Baconator", false, "2019-07-28 16:20:00"),
   ("Quarter Pounder", false, "2019-07-30 16:20:01"),
-  ("McRoyal", false, "2019-07-27 04:20:20"),
-  ("Double Double", true, "2019-07-26 16:20:16");
+  ("McRip", false, "2019-07-27 04:20:20"),
+  ("Jr. Bacon Cheeseburger", true, "2019-07-26 16:20:16");
